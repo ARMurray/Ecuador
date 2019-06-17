2164d43ba4808396d4f41015931965ae68b7e78d
Andrew <ARMurray@live.unc.edu>
2019-06-17 12:53:38 -0400

    test plot for Vaisala 1

--- Vaisala_Test_Andrew.R
+++ Vaisala_Test_Andrew.R
@@ -1,4 +1,5 @@
 library(here)
+library(ggplot2)
 
 
 # Import the Vaisala data for a specific day
@@ -9,6 +10,7 @@ V4 <- read.csv(here("FieldData","VB4_06132019.csv"))
 
 
 # Convert time to POSIXct in a new column called "DateTime"
+V1 <- V1[1:5000,]
 V1$DateTime <- paste0(V1$Date," ",V1$Time)
 V1$DateTime <- as.POSIXct(V1$DateTime, format = "%m/%d/%Y %H:%M:%S")
 
@@ -22,3 +24,6 @@ V4$DateTime <- paste0(V4$Date," ",V4$Time)
 V4$DateTime <- as.POSIXct(V4$DateTime, format = "%m/%d/%Y %H:%M:%S")
 
 
+V1Plot <- ggplot(V1, aes(x = DateTime, y = Volts))+
+  geom_line()
+V1Plot
