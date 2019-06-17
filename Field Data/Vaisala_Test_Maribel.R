1b264247112365918bb6b45b562eecb6011e69f1
Maribel Herrera <mari.herrera6983@gmail.com>
2019-06-17 12:45:19 -0400

    Maribel Test

--- Field Data/Vaisala_Test_Maribel.R
+++ Field Data/Vaisala_Test_Maribel.R
@@ -10,7 +10,13 @@ try<- V1
 try$Date <-as.POSIXct(as.character(try$Date))
 
 V1$DateTime <-paste0(V1$Date," ", V1$Time)
-V2$DateTime <-as.POSIXct(V2$DateTime, format = "%m/%d/%Y %H:%M:%OS")
-V3$DateTime <-as.POSIXct(V3$DateTime, format = "%m/%d/%Y %H:%M:%OS")
-V4$DateTime <-as.POSIXct(V4$DateTime, format = "%m/%d/%Y %H:%M:%OS")
 V1$DateTime <-as.POSIXct(V1$DateTime, format = "%m/%d/%Y %H:%M:%OS")
+
+V2$DateTime <-paste0(V2$Date," ", V1$Time)
+V2$DateTime <-as.POSIXct(V2$DateTime, format = "%m/%d/%Y %H:%M:%OS")
+
+V3$DateTime <-paste0(V3$Date," ", V1$Time)
+V3$DateTime <-as.POSIXct(V3$DateTime, format = "%m/%d/%Y %H:%M:%OS")
+
+V4$DateTime <-paste0(V4$Date," ", V1$Time)
+V4$DateTime <-as.POSIXct(V4$DateTime, format = "%m/%d/%Y %H:%M:%OS")
