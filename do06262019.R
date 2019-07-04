d1a0dd28b4d96d139b0af46c6789d50c3535a427
unknown <yancyestacio@yahoo.com>
2019-07-04 15:00:27 -0400

    Some bullsheski is happening

---
+++ do06262019.R
@@ -0,0 +1,16 @@
+library(here)
+library(readxl)
+
+date <- "06262019"
+
+data <- read_xls(here("FieldData/DO", paste0("DO_", date ,".xls")))
+data <- data[,2:4]
+colnames(data) <- c("Time", "mg/L", "TempC")
+
+write.csv(data, ("FieldData/DO", paste0("DO_", date, ".csv")))
+
+dTable <- read.table(here("FieldData/DO", "DO_06262019.csv"))
+
+dTable$datetime <- as.POSIXct(as.character(paste0(dTable$Month, "/", dTable$Day, "/", dTable$Year, " " dTable$Time)), format = "%m/%d/%Y %H:%M:%OS")
+
+                     
