e26318590b8429c4c84edc601ce5c2e6d18522e6
Maribel Herrera <mari.herrera6983@gmail.com>
2019-07-04 18:14:17 -0400

    final gm70

--- Sensor_Workflows/GM70_Processing.R
+++ Sensor_Workflows/GM70_Processing.R
@@ -16,20 +16,19 @@ GM70_Lon <- "Input Longitude in decimal degrees"
 # Import the Vaisala data for the date specified above.
 GM70 <- read.csv(here("FieldData/GM70HandheldVaisala",paste0("GM70_",date,".csv")))
 
+#change co2..ppm column name to just ppm
+
+GM70$ppm <- GM70$CO2..ppm
 
 # Convert time to POSIXct in a new column called "DateTime"
-V1 <- V1[1:5000,]
-V1$DateTime <- paste0(V1$Date," ",V1$Time)
-V1$DateTime <- as.POSIXct(V1$DateTime, format = "%m/%d/%Y %H:%M:%S")
-V1 <- V1%>%
-  select(DateTime,Volts)
+GM70$DateTime <- paste0(GM70$X)
+GM70$DateTime <- as.POSIXct(GM70$DateTime, format = "%m/%d/%Y %H:%M:%S")
+GM70 <- GM70%>%
+  select(DateTime, ppm)
 
 
-dataMerge <- rbind(V1,V2,V3,V4)
 
-dataMerge$VID <- as.factor(dataMerge$VID)
-
-
-outPlot <- ggplot(dataMerge, aes(x = DateTime, y = Volts, group = VID))+
-  geom_line(aes(col=VID))
+outPlot <- ggplot(GM70, aes(x = DateTime, y = ppm))+ 
+  geom_point()+
+  geom_line()
 outPlot
