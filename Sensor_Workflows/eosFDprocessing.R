1ea356214eb91c9f4fbfb1399c56df17d3d57a0e
Andrew <ARMurray@live.unc.edu>
2019-07-04 07:37:09 -0500

    Updated eosFD script to focus it on processing

--- Sensor_Workflows/eosFDprocessing.R
+++ Sensor_Workflows/eosFDprocessing.R
@@ -1,81 +1,26 @@
 library(here)
+library(ggplot2)
+library(dplyr)
 
+# *** SET THE DATE ***
 date<- "06182019"
-# eos 1 data
 
+# eos1 data
 eos1 <- read.csv(here("FieldData",paste0("eos1_",date,".csv")))
+eos1$Year <- paste0("20",eos1$Year)
+eos1$dateTime <- as.POSIXct(as.character(paste0(eos1$Month,"/",eos1$Day,"/",eos1$Year," ",eos1$Time)), format = "%m/%d/%Y %H:%M:%OS")
+eos1 <- eos1%>%
+  select(dateTime,Flux,Temperature..C.,CO2.Soil..ppm.,
+         CO2.Soil.STD..ppm.,CO2.ATM..ppm.,CO2.ATM.STD..ppm.,Mode)
+colnames(eos1) <- c("DateTime","Flux_eos1","Temp_C_eos1","CO2_Soil_ppm_eos1","CO2_Soil_STD_ppm_eos1",
+                    "CO2_ATM_ppm_eos1","CO2_ATM_STD_ppm_eos1","Mode")
 
-Time<- eos1$Time
-Flux <- eos1$Flux
-eos1$Time <- as.POSIXct(as.character(eos1$Time), format = "%H:%M:%OS")
-
-
-#make a table with just flux and time of desired data
- 
-new<- data.frame(Time = Time[1398:1439], Flux = Flux[1398:1439])
-new$Time <- as.POSIXct(as.character(new$Time), format = "%H:%M:%OS")
-new
-#install.packages("ggplot2")
-library("ggplot2")
-
-
-plot <- ggplot(new)+
-  geom_line(aes(x= Time, y= Flux, group=1)) 
-plot
-
-#add injection points to the frame
-injection1 <- data.frame(Time= new$Time[29:31], Flux= new$Flux[29:31])
-injection2 <- data.frame(Time = new$Time[38:40], Flux = new$Flux[38:40])
-
-
-plot <- plot+
-  geom_point(data=injection1, aes(x=Time, y= Flux), colour = 'red', size = 3)+
-  geom_point(data=injection2, aes(x=Time, y= Flux), colour = 'blue', size = 3)
-plot
-
-#eos2 data 
+# eos2 data 
 eos2 <- read.csv(here("FieldData", "EosFD",paste0("eos2_",date,".csv")))
-
 eos2$Time <- as.POSIXct(as.character(eos2$Time), format = "%H:%M:%OS")
-Time<- eos2$Time
-Flux <- eos2$Flux
 
-eos2
-#make a table with just flux and time of desired data
+# Find the first and last time stamps that both eosFD sensors have data
 
-new2<- data.frame(Time = Time[1374:1415], Flux = Flux[1374:1415])
-new2
+# Merge the data into a single data frame
 
-#add second eos data to plot
-plot <- plot + geom_line(data=new2, aes(x= Time, y= Flux)) 
-plot
-
-
-# Plot regression and find residuals
-# Merge eos data into one table
-colnames(new) <- c("Time","eos1")
-colnames(new2) <- c("Time","eos2")
-merge <- merge(new,new2)
-
-lm <- lm(eos2~eos1, data = merge)
-
-summary(lm)
-
-plot(lm)
-
-# Find the residuals
-
-residuals <- as.data.frame(resid(lm))
-
-# Add residuals to data frame with flux data
-merge <- cbind(merge,residuals)
-
-pulls <- c(29,30,31,38,39,40)
-
-pResid <- residuals[pulls,]
-
-# Pull residuals
-
-pResidAvg <- mean(abs(pResid))
-
-residAvg <- mean(abs(residuals$`resid(lm)`))
+# Export the data
