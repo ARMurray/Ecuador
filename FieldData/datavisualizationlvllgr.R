342b8223ddbb78c41c8b5df4d592f1172fda082a
unknown <yancyestacio@yahoo.com>
2019-07-08 12:18:25 -0400

    reorg

--- FieldData/datavisualizationlvllgr.R
+++
@@ -1,9 +0,0 @@
-library("ggplot2")
-install.packages("colorspace")
-library("gapminder")
-library(here)
-
-data <- read.csv(here("FieldData","lvllgr06182019rogersroad.csv"), skip = 11)
-
-plot <- ggplot(new)+
-  geom_line(aes(x= Time, y= LEVEL))     
