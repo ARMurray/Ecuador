065c1f0cdbd09d82f407d9f68fd21c3f5b0b52d0
mraisle <mraisle@live.unc.edu>
2019-07-14 19:11:23 -0500

    workin on gps

--- Sensor_Workflows/firstgpsworkflowattempt.R
+++ Sensor_Workflows/firstgpsworkflowattempt.R
@@ -2,6 +2,15 @@ install.packages("tmaptools")
 library(tmaptools)
 library(ggplot2)
 
+
+# This script is for the processing of data collected from the GPS Garmen e20x 
+
+# Enter the date you want to process in the format 'mmddyyyy'
+date <- '07062019'
+
+# Import the Vaisala data for the date specified above.
+waypoints6july <- read.csv(here("FieldData/GM70HandheldVaisala",paste0("GM70_",date,".csv")))
+
+
 july6points <- read_GPX("C:/Users/hmir/Desktop/7-6 and 7-5 gps data/Waypoints_06-JUL-19.gpx", layers = "waypoints")
-write.csv(july6points, "C:/Users/hmir/Desktop/7-6 and 7-5 gps data/july6points.csv")
-july6waypoint <- read.csv("C:/Users/hmir/Desktop/7-6 and 7-5 gps data/july6points.csv")
+july6waypoint <- read.csv("C:/Users/hmir/Documents/Ecuador/FieldData/GPS/july6points.csv")
