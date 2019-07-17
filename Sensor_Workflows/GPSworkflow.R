618a03b4bf05e1ea54a6b685d646f6e9c65fcd3a
mraisle <mraisle@live.unc.edu>
2019-07-17 17:23:39 -0500

    ramen > pb&j

--- Sensor_Workflows/GPSworkflow.R
+++ Sensor_Workflows/GPSworkflow.R
@@ -8,8 +8,8 @@ library(dplyr)
 # This script is for the processing of data collected from the GPS Garmen e20x 
 
 # Enter the date you want to process in the format 'mmddyyyy'
-date <- '06-JUL-19'
-date1 <- '070619'
+date <- '16-JUL-19'
+date1 <- '071619'
 
 # Import the GPS data for the date specified above.
 GPS1 <- read_GPX(here("FieldData/GPS/GPS1", paste0("Waypoints_",date,".gpx")), layers = "waypoints")
@@ -23,10 +23,11 @@ GPS1 <- GPS1 %>%
 
 #Delete excess points specific to this set of data, this is specific to each set of data and needs to be changed every time. 
 
-GPS1 <- GPS1[-c(3,11,13,54:92), ]
+GPS1 <- GPS1[-c(46), ]
 
-#GPS1$name <- as.character(GPS1$name)
-#GPS1$name[12] <- "WPUD9"
+GPS1$name <- as.character(GPS1$name)
+GPS1$name[46] <- "SSOutlet"
+
 
 write_shape(GPS1,here("FieldData/GPS", paste0("GPS_",date1, ".shp")))
 
