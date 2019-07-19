1d5f9232e7449efeb706311efd68291a58139c6c
mraisle <mraisle@live.unc.edu>
2019-07-19 11:09:44 -0500

    gps schtuff

--- Sensor_Workflows/GPSworkflow.R
+++ Sensor_Workflows/GPSworkflow.R
@@ -8,8 +8,8 @@ library(dplyr)
 # This script is for the processing of data collected from the GPS Garmen e20x 
 
 # Enter the date you want to process in the format 'mmddyyyy'
-date <- '16-JUL-19'
-date1 <- '071619'
+date <- '05-JUL-19'
+date1 <- '070519'
 
 # Import the GPS data for the date specified above.
 GPS1 <- read_GPX(here("FieldData/GPS/GPS1", paste0("Waypoints_",date,".gpx")), layers = "waypoints")
@@ -23,10 +23,21 @@ GPS1 <- GPS1 %>%
 
 #Delete excess points specific to this set of data, this is specific to each set of data and needs to be changed every time. 
 
-GPS1 <- GPS1[-c(46), ]
+GPS1 <- GPS1[-c(8,12,24), ]
 
 GPS1$name <- as.character(GPS1$name)
-GPS1$name[46] <- "SSOutlet"
+GPS1$name[3] <- "HighWaterfall"
+
+#Let's make a new column in all of the files that have the proper attritbution 
+#let's set that up 
+#here's what it'll be:
+#streamhike
+#focuswatershed
+#WestCatchment
+#GeneralGeog
+#EOStransects
+#Levelandbarro
+#Synoptics 
 
 
 write_shape(GPS1,here("FieldData/GPS", paste0("GPS_",date1, ".shp")))
--- Sensor_Workflows/mergingGPSdata.R
+++ Sensor_Workflows/mergingGPSdata.R
@@ -9,6 +9,21 @@ library(dplyr)
 #Howdy parnters. Let's use this space to put some GPs points together and make them look #coolll
 
 
-#First we're going to bring in all the .shp files we want to combine 
+#First I need to make a new column in all of the files that have the proper attritbution 
+#let's set that up 
+#here's what it'll be:
+#streamhike
+#focuswatershed
+#WestCatchment
+#GeneralGeog
+#EOStransects
+#Levelandbarro
+#Synoptics 
 
-Raw <- read_shape()
+#Let's import it one by one 
+
+date <- '070519'
+
+GPS1 <- read_shape(here("FieldData/GPS", paste0("GPS_",date,".shp")))
+GPS1$Category <- "GeneralGeog"
+GPS1$Category[12] <- ""
