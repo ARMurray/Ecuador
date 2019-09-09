382b495883a0114563fd64eb0e6fe6bc2662f118
mraisle <mraisle@mraisle-pc.wireless-1x.unc.edu>
2019-09-09 00:21:04 -0400

    ta-da

--- Picarro/EOSTransects/EOS Transects Picarro .R
+++ Picarro/EOSTransects/EOS Transects Picarro .R
@@ -3,54 +3,55 @@ library(ggplot2)
 library(here)
 library(dplyr)
 
-date <- 'July23'
+date <- 'Aug9'
 #Ok first let's bring in the data and clean her up a bit 
-July23.1 <- read.table(here("2019 picarro/07/23/CFIDS2089-20190723-105943Z-DataLog_User.dat"), header=TRUE)
+Aug9.1 <- read.table(here("2019 picarro/08/09/CFIDS2089-20190809-112009Z-DataLog_User.dat"), header=TRUE)
 
 
-July23.1 <- July23.1 %>%
+Aug9.1 <- Aug9.1 %>%
   select(DATE, TIME, X12CO2, Delta_Raw_iCO2)
-July23.1 <- July23.1[c(1401:2067), ]
+Aug9.1 <- Aug9.1[c(413:2069), ]
 
-write.csv(July23.1,here("Picarro/EOSTransects/072219/", paste0(date,".1", ".csv")))
+write.csv(Aug9.1,here("Picarro/EOSTransects/080819/", "Aug9.1.csv"))
 
 #next file
-July23.2 <- read.table(here("2019 picarro/07/23/CFIDS2089-20190723-112952Z-DataLog_User.dat"), header=TRUE)
+Aug9.2 <- read.table(here("2019 picarro/08/09/CFIDS2089-20190809-115017Z-DataLog_User.dat"), header=TRUE)
 
-July23.2 <- July23.2 %>%
+Aug9.2 <- Aug9.2 %>%
   select(DATE, TIME, X12CO2, Delta_Raw_iCO2)
 
+Aug9.2 <- Aug9.2[c(1:1427), ]
 
-write.csv(July23.2,here("Picarro/EOSTransects/072219/", "July23.2.csv"))
+write.csv(Aug9.2,here("Picarro/EOSTransects/080819/", "Aug9.2.csv"))
 
 #last one 
-July23.3 <- read.table(here("2019 picarro/07/23/CFIDS2089-20190723-120001Z-DataLog_User.dat"), header=TRUE)
+Aug9.3 <- read.table(here("2019 picarro/08/09/CFIDS2089-20190730-122432Z-DataLog_User.dat"), header=TRUE)
 
-July23.3 <- July23.3 %>%
+Aug9.3 <- Aug9.3 %>%
   select(DATE, TIME, X12CO2, Delta_Raw_iCO2)
 
-July17.3 <- July17.3[c(1:1527), ]
+Aug9.3 <- Aug9.3[c(1:455), ]
 
 
-write.csv(July23.3,here("Picarro/EOSTransects/072219/", "July23.3.csv"))
+write.csv(Aug9.3,here("Picarro/EOSTransects/080819/", "Aug9.3.csv"))
 
 #Merge all them files together 
 
 ### Get a list of all the files
-July23_Files <- list.files(here::here("Picarro/EOSTransects/072219"),pattern = 'July23')
-list(July23_Files)
+Aug9_Files <- list.files(here::here("Picarro/EOSTransects/080819"),pattern = 'Aug9')
+list(Aug9_Files)
 
 #create an empty data.frame
-allJuly23Data <- read.csv(here::here("Picarro/EOSTransects/072219",July23_Files[1]))
+allAug9Data <- read.csv(here::here("Picarro/EOSTransects/080819",Aug9_Files[1]))
 
 
-for(i in 2:length(July23_Files)){
-  file <- July23_Files[i]
-  data <- read.csv(here::here("Picarro/EOSTransects/072219",file))
-  allJuly23Data <- rbind(allJuly23Data,data)}
+for(i in 2:length(Aug9_Files)){
+  file <- Aug9_Files[i]
+  data <- read.csv(here::here("Picarro/EOSTransects/080819",file))
+  allAug9Data <- rbind(allAug9Data,data)}
   
 #Now save the dang thang 
 
-write.csv(allJuly23Data,here("Picarro/EOSTransects/072219/allJuly23data.csv"))
+write.csv(allAug9Data,here("Picarro/EOSTransects/080819/allAug9data.csv"))
 
 
