26e149e4c9b23226c3c6fe730b00801d58385aeb
Schneider <chloe33@ad.unc.edu>
2019-09-25 16:46:41 -0400

    c

--- Analysis/practiceVelocity.R
+++ Analysis/practiceVelocity.R
@@ -10,5 +10,6 @@ syn5 <- v$Syn6_081219
 dis <- v$Distance
 
 
-plot <- ggplot(v) + geom_point(aes(x= Distance, y= Syn1_071819), color = "purple", size = 2)
+
+plot <- ggplot(v) + geom_point(aes(x= Distance, y= Syn1_071819), color = "purple", size = 2) +
 plot
