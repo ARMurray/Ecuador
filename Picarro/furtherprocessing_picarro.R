3dca2f389723095055959baad1d7fbdd002025b5
Raisle <mraisle@ad.unc.edu>
2019-09-30 09:58:46 -0400

    whops

--- Picarro/furtherprocessing_picarro.R
+++ Picarro/furtherprocessing_picarro.R
@@ -6,8 +6,10 @@ library(plotly)
 library(wesanderson)
 
 #open up the cleaned file that you want to work with 
-alljuly17 <- read.csv(here("Picarro/EOSTransects/071619/alljuly17Data.csv"))
+alljuly17 <- read.csv(here("Ecuador/Picarro/EOSTransects/071619/alljuly17Data.csv"))
 
+#make posic.ct, posixct again
+alljuly17$PosixCT <- as.POSIXct(alljuly17$PosixCT)
 
 #Do an initial plot of C02 
 plot <- ggplot(alljuly17)+
@@ -21,9 +23,13 @@ plot <- ggplot(Subsjuly17)+
 geom_point(aes(x= PosixCT, y= Delta_Raw_iCO2)) +
 labs(x = "Time", y = "Delta_Raw") +
 ggtitle("July17 EOS Transect Pulls") 
-ggplotly(plot)
+
 plot
+ggplotly(plot)
 
 
-Subsjuly17 <- alljuly17[c(70400:99100), ]
+Subsjuly17 <- alljuly17[c(70400:83600), ]
+
+col1 <- alljuly17[ which(alljuly17$PosixCT > "2019-07-17 18:11:00"
+                                     & alljuly17$PosixCT < "2019-07-17 18:14:15"), ]
 
