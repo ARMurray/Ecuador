5d7bae257f892658b353d630f9ee42ac6ae680dd
NehemiahDaGoat <Nehemiah@live.unc.edu>
2019-10-14 14:25:45 -0400

    commit

---
+++ Calculating.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(ggplot2)
+
+#Calculating Henry's Constant
+##Kh=Kh at SATP * e^(D(1/T(Syn))-(1/T at SATP)) Kh at SATP= .035 mol/L D=2400 T at SATP=
+Kh<-.035*e^(2400(1/))
+
+#Uploading Water Level
+
+
+
+#Calculating Flux from Vaisala
