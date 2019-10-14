b4da894b2e38a7b383699679118d0355ac8a3eca
mraisle <mraisle@mraisle-pc.wireless-1x.unc.edu>
2019-10-14 12:54:43 -0400

    boop

--- Picarro/EOSTransects/stderror_eostransects.R
+++ Picarro/EOSTransects/stderror_eostransects.R
@@ -12,5 +12,32 @@ comboecuavg <- read.csv(here("Picarro/EOSTransects/comboecuavg.csv"))
 
 rawdata <- read.csv(here("Picarro/EOSTransects/allecusamples_raw.csv"))
 
-Samplenumber <-- data.frame()
+samplenumber <- read.csv(here("Picarro/EOSTransects/samplenumbers.csv"))
+                         
+totalecu <- cbind(allecuavg, samplenumber)
+
+#ok now let's calculate standard error 
+stderror <- data.frame(totalecu$StdDev_iCO2/sqrt(totalecu$sampleNumber))
+stderror$stderror <-- as.numeric(stderror$totalecu.StdDev_iCO2.sqrt.totalecu.sampleNumber.)
+stderror <- stderror %>%
+  select(stderror)
+
+#let's add that to the full table
+
+totalecu <- cbind(totalecu, stderror)
+
+#95% confidence intervals of the mean
+c(totalecu$Avg_iCO2-(2*stderror$stderror), totalecu$Avg_iCO2+(2*stderror$stderror))
+
+totalecu$lowbound <- c(totalecu$Avg_iCO2-(2*stderror$stderror))
+totalecu$highbound <- c(totalecu$Avg_iCO2+(2*stderror$stderror))
+
+#reorg table 
+totalecu<- totalecu %>%
+  select(Day, Sample, sampleNumber, Avg_iCO2, StdDev_iCO2, stderror, lowbound, highbound)
+
+#save this 
+
+write.csv(totalecu, here("Picarro/EOSTransects/statsecusamples.csv"))
+
 
