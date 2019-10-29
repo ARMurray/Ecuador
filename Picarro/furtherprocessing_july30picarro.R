a75b7678f36e4736ee311bc07a6b70b8e962a312
Raisle <mraisle@ad.unc.edu>
2019-10-29 12:05:47 -0400

    t

--- Picarro/furtherprocessing_july30picarro.R
+++ Picarro/furtherprocessing_july30picarro.R
@@ -114,3 +114,32 @@ July30avg <- data.frame("Day" = "July30", "Avg_iCO2" = mean(ecutogether$Delta_30
 
 write.csv(July30avg, here("Picarro/EOSTransects/072919/", "July30alldayecuavg.csv"))
 
+
+#ok we need to get a summary table just for the collars for each day 
+
+#ok now let's find the average and std. deviation for each bag pull and put that into a new data frame
+
+col1avg <- data.frame("Sample" = "col1", "Avg_iCO2" = mean(col1$Delta_30s_iCO2), "StdDev_iCO2" = sd(col1$Delta_30s_iCO2))
+col2avg <- data.frame("Sample" = "col2", "Avg_iCO2" = mean(col2$Delta_30s_iCO2), "StdDev_iCO2" = sd(col2$Delta_30s_iCO2))
+col3avg <- data.frame("Sample" = "col3", "Avg_iCO2" = mean(col3$Delta_30s_iCO2), "StdDev_iCO2" = sd(col3$Delta_30s_iCO2))
+col4avg <- data.frame("Sample" = "col4", "Avg_iCO2" = mean(col4$Delta_30s_iCO2), "StdDev_iCO2" = sd(col4$Delta_30s_iCO2))
+col5avg <- data.frame("Sample" = "col5", "Avg_iCO2" = mean(col5$Delta_30s_iCO2), "StdDev_iCO2" = sd(col5$Delta_30s_iCO2))
+col6avg <- data.frame("Sample" = "col6", "Avg_iCO2" = mean(col6$Delta_30s_iCO2), "StdDev_iCO2" = sd(col6$Delta_30s_iCO2))
+col7avg <- data.frame("Sample" = "col7", "Avg_iCO2" = mean(col7$Delta_30s_iCO2), "StdDev_iCO2" = sd(col7$Delta_30s_iCO2))
+col8avg <- data.frame("Sample" = "col8", "Avg_iCO2" = mean(col8$Delta_30s_iCO2), "StdDev_iCO2" = sd(col8$Delta_30s_iCO2))
+col5testavg <- data.frame("Sample" = "col5test", "Avg_iCO2" = mean(col5test$Delta_30s_iCO2), "StdDev_iCO2" = sd(col5test$Delta_30s_iCO2))
+
+
+sumjuly30_30 <- rbind(col1avg, col2avg, col3avg, col4avg, col5avg, col6avg, col7avg, col8avg, col5testavg)
+
+write.csv(sumjuly30_30, here("Picarro/EOSTransects/072919/", "sumjuly30_30sec_uncorrected.csv"))
+
+
+#ok now let's do the correction 
+
+sumjuly30_30$Correction <- as.numeric("-0.003928632")
+sumjuly30_30$CorrectedAverage <- as.numeric(c(sumjuly30_30$Avg_iCO2 + sumjuly30_30$Correction))
+
+write.csv(sumjuly30_30, here("Picarro/EOSTransects/072919/", "sumjuly30_30sec_corrected.csv"))
+
+
