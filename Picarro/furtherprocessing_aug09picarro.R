5259ee61a682e22be432fe477e1347edd4f539ca
Raisle <mraisle@ad.unc.edu>
2019-10-29 12:08:50 -0400

    09

--- Picarro/furtherprocessing_aug09picarro.R
+++ Picarro/furtherprocessing_aug09picarro.R
@@ -110,3 +110,35 @@ Aug09avg <- data.frame("Day" = "Aug09", "Avg_iCO2" = mean(ecutogether$Delta_30s_
 
 write.csv(Aug09avg, here("Picarro/EOSTransects/071619/", "Aug09alldayecuavg.csv"))
 
+
+#ok we need to get a summary table just for the collars for each day 
+
+#ok now let's find the average and std. deviation for each bag pull and put that into a new data frame
+
+
+
+col1avg <- data.frame("Sample" = "col1", "Avg_iCO2" = mean(col1$Delta_30s_iCO2), "StdDev_iCO2" = sd(col1$Delta_30s_iCO2))
+col2avg <- data.frame("Sample" = "col2", "Avg_iCO2" = mean(col2$Delta_30s_iCO2), "StdDev_iCO2" = sd(col2$Delta_30s_iCO2))
+col3avg <- data.frame("Sample" = "col3", "Avg_iCO2" = mean(col3$Delta_30s_iCO2), "StdDev_iCO2" = sd(col3$Delta_30s_iCO2))
+col4avg <- data.frame("Sample" = "col4", "Avg_iCO2" = mean(col4$Delta_30s_iCO2), "StdDev_iCO2" = sd(col4$Delta_30s_iCO2))
+col5avg <- data.frame("Sample" = "col5", "Avg_iCO2" = mean(col5$Delta_30s_iCO2), "StdDev_iCO2" = sd(col5$Delta_30s_iCO2))
+col6avg <- data.frame("Sample" = "col6", "Avg_iCO2" = mean(col6$Delta_30s_iCO2), "StdDev_iCO2" = sd(col6$Delta_30s_iCO2))
+col7avg <- data.frame("Sample" = "col7", "Avg_iCO2" = mean(col7$Delta_30s_iCO2), "StdDev_iCO2" = sd(col7$Delta_30s_iCO2))
+col8avg <- data.frame("Sample" = "col8", "Avg_iCO2" = mean(col8$Delta_30s_iCO2), "StdDev_iCO2" = sd(col8$Delta_30s_iCO2))
+
+
+
+sumaug09_30 <- rbind(col1avg, col2avg, col3avg, col4avg, col5avg, col6avg, col7avg, col8avg)
+
+write.csv(sumaug09_30, here("Picarro/EOSTransects/080819/", "sumaug09_30sec_uncorrected.csv"))
+
+
+#ok now let's do the correction 
+
+sumaug09_30$Correction <- as.numeric("-2.393811676")
+sumaug09_30$CorrectedAverage <- as.numeric(c(sumaug09_30$Avg_iCO2 + sumaug09_30$Correction))
+
+write.csv(sumaug09_30, here("Picarro/EOSTransects/080819/", "sumaug09_30sec_corrected.csv"))
+
+
+
