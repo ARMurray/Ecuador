e59a583a543857f3f8a0b82e089231906d76aaa4
Raisle <mraisle@ad.unc.edu>
2019-11-11 13:22:35 -0500

    h

--- Sensor_Workflows/c6_processing.R
+++ Sensor_Workflows/c6_processing.R
@@ -26,3 +26,38 @@ allC6Data$DateTime <- as.POSIXct(as.character(allC6Data$DateTime), format = "%m/
 #write a new file and put it in folder for processig
 
 write.csv(allC6Data, here("data_4_analysis/C6.csv"))
+
+oneday <- read.csv(here("FieldData/C6/C6_071119.csv"))
+
+c6 <- read.csv(here("data_4_analysis/c6.csv"))
+
+#make posic.ct, posixct again
+c6$DateTime <- as.POSIXct(c6$DateTime)
+
+
+c6short <- c6[c(1:661), ]
+
+
+
+#let's try to make a simple plot 
+plot <- ggplot(c6short)+
+  geom_point(aes(x= DateTime, y= CDOM_ppb), color="blue")
+plot
+ggplotly(plot)
+
+plot2 <- ggplot(c6short)+
+  geom_point(aes(x= DateTime, y= Turbidity_NTU), color="red")
+plot2
+ggplotly(plot2)
+
+plot3 <- ggplot(c6short)+
+  geom_point(aes(x= DateTime, y= Chlorophylla_ug.L), color="green")
+plot3
+ggplotly(plot3)
+
+plot4 <- ggplot(c6short)+
+  geom_point(aes(x= DateTime, y= Phycocyanin_ppb), color="purple")
+plot4
+ggplotly(plot4)
+  
+  
