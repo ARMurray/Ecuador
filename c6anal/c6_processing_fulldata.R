2238db4b5f94998beacff02fecf8b9c244542f07
Raisle <mraisle@ad.unc.edu>
2019-11-18 11:20:17 -0500

    f

--- c6anal/c6_processing_fulldata.R
+++ c6anal/c6_processing_fulldata.R
@@ -3,8 +3,7 @@ library(ggplot2)
 library(dplyr)
 library(viridis)
 library(plotly)
-
-install.packages("grid")
+library(grid)
 
 ### load the giant stream file 
 
@@ -24,22 +23,44 @@ write.csv(c6time, here("c6anal/", "alldatac6times.csv"))
 
 #basic graph of all four 
 
+#set same x-scale for each 
+x_min <- as.POSIXct("2019-07-11 11:45:00")
+x_max <- as.POSIXct("2019-07-19 12:59:00")
+
+largernumbers <- element_text(face = "bold", size = 20)
+
+#do basic plot of each 
 plot <- ggplot(c6time)+
-  geom_point(aes(x= DateTime, y= CDOM_ppb), color="blue")
+  geom_point(aes(x= DateTime, y= CDOM_ppb), color= "#27223c")
+#plot <- plot + scale_x_continuous(limits=c(x_min, x_max))
+plot <- plot + theme(axis.text.y= largernumbers)
 ggplotly(plot)
 
 plot2 <- ggplot(c6time)+
-  geom_point(aes(x= DateTime, y= Turbidity_NTU), color="red")
+  geom_point(aes(x= DateTime, y= Turbidity_NTU), color="#d1362f")
+#plot2 <- plot2 + scale_x_continuous(limits=c(x_min, x_max))
+plot2 <- plot2 +theme(axis.text.y= largernumbers)
+
 ggplotly(plot2)
 
 plot3 <- ggplot(c6time)+
-  geom_point(aes(x= DateTime, y= Chlorophylla_ug.L), color="green")
+  geom_point(aes(x= DateTime, y= Chlorophylla_ug.L), color="#2e604a")
+#plot3 <- plot3 + scale_x_continuous(limits=c(x_min, x_max))
+plot3 <- plot3 + theme(axis.text.y= largernumbers)
+
 ggplotly(plot3)
 
 plot4 <- ggplot(c6time)+
-  geom_point(aes(x= DateTime, y= Phycocyanin_ppb), color="purple")
+  geom_point(aes(x= DateTime, y= Phycocyanin_ppb), color="#dbb165")
+#plot4 <- plot4 + scale_x_continuous(limits=c(x_min, x_max))
+plot4 <- plot4 + theme(axis.text.y= largernumbers)
+
 ggplotly(plot4)
 
+
+#put  them all together 
+grid.newpage()
+grid.draw(rbind(ggplotGrob(plot), ggplotGrob(plot2), ggplotGrob(plot3), ggplotGrob(plot4)))
 
 
 pbetter2 <- ggplot(sumalltransects, aes(x= Sample, y= CorrectedAverage, color=day))+
