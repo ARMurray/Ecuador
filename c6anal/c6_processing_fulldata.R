02f831977d3d1f938af80d85d2b76c2293a795f8
Raisle <mraisle@ad.unc.edu>
2019-11-18 12:40:51 -0500

    f

--- c6anal/c6_processing_fulldata.R
+++ c6anal/c6_processing_fulldata.R
@@ -27,32 +27,32 @@ write.csv(c6time, here("c6anal/", "alldatac6times.csv"))
 x_min <- as.POSIXct("2019-07-11 11:45:00")
 x_max <- as.POSIXct("2019-07-19 12:59:00")
 
-largernumbers <- element_text(face = "bold", size = 20)
+largernumbers <- element_text(face = "bold", size = 14)
 
 #do basic plot of each 
 plot <- ggplot(c6time)+
-  geom_point(aes(x= DateTime, y= CDOM_ppb), color= "#27223c")
-#plot <- plot + scale_x_continuous(limits=c(x_min, x_max))
+  geom_point(aes(x= DateTime, y= CDOM_ppb), color= "#27223c")+
+  ggtitle("Colored Dissolved Organic Matter")
 plot <- plot + theme(axis.text.y= largernumbers)
 ggplotly(plot)
 
 plot2 <- ggplot(c6time)+
-  geom_point(aes(x= DateTime, y= Turbidity_NTU), color="#d1362f")
-#plot2 <- plot2 + scale_x_continuous(limits=c(x_min, x_max))
+  geom_point(aes(x= DateTime, y= Turbidity_NTU), color="#d1362f")+
+  ggtitle("Turbidity")
 plot2 <- plot2 +theme(axis.text.y= largernumbers)
 
 ggplotly(plot2)
 
 plot3 <- ggplot(c6time)+
-  geom_point(aes(x= DateTime, y= Chlorophylla_ug.L), color="#2e604a")
-#plot3 <- plot3 + scale_x_continuous(limits=c(x_min, x_max))
+  geom_point(aes(x= DateTime, y= Chlorophylla_ug.L), color="#2e604a")+
+  ggtitle("Chlorophyll A")
 plot3 <- plot3 + theme(axis.text.y= largernumbers)
 
 ggplotly(plot3)
 
 plot4 <- ggplot(c6time)+
-  geom_point(aes(x= DateTime, y= Phycocyanin_ppb), color="#dbb165")
-#plot4 <- plot4 + scale_x_continuous(limits=c(x_min, x_max))
+  geom_point(aes(x= DateTime, y= Phycocyanin_ppb), color="#dbb165")+
+  ggtitle("Phycocyanin")
 plot4 <- plot4 + theme(axis.text.y= largernumbers)
 
 ggplotly(plot4)
@@ -63,11 +63,65 @@ grid.newpage()
 grid.draw(rbind(ggplotGrob(plot), ggplotGrob(plot2), ggplotGrob(plot3), ggplotGrob(plot4)))
 
 
-pbetter2 <- ggplot(sumalltransects, aes(x= Sample, y= CorrectedAverage, color=day))+
-  geom_point(size=8)+
-  xlab('Collar') +
-  ylab('Delta30_Avg') +
-  ggtitle("EOS Delta30 i_CO2 Corrected Summary") +
-  scale_color_manual(values=wes_palette("Zissou1", n=6, type = c("continuous")))+
-  theme(legend.position = "right")
-pbetter2
+
+#ok now let's try to add rainfall to this 
+
+
+plot5 <- ggplot(c6time)+
+  geom_bar(aes(x= DateTime, y = ppt_mm), color = "#1dace8", stat="identity")+
+  ggtitle("Five Minute Precipitation")
+plot5 <- plot5 + theme(axis.text.y= largernumbers)
+ggplotly(plot5)
+
+
+plot6 <- ggplot(c6time)+
+  geom_bar(aes(x= DateTime, y = ppt24Tot), color = "#1dace8", stat="identity")+
+  ggtitle("Previous 24 Hours of Precipitation")
+plot6 <- plot6 + theme(axis.text.y= largernumbers)
+ggplotly(plot6)
+
+
+#let's put them all together
+
+#first 5 min rainfall 
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(plot5),ggplotGrob(plot), ggplotGrob(plot2), ggplotGrob(plot3), ggplotGrob(plot4)))
+
+
+
+#then 24 hour rainfall
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(plot6),ggplotGrob(plot), ggplotGrob(plot2), ggplotGrob(plot3), ggplotGrob(plot4)))           
+
+
+# Alright now let's try to roll in c02 data 
+
+x_min <- as.POSIXct("2019-07-12 13:00:0")
+x_max <- as.POSIXct("2019-07-12 16:00:00")
+
+#plot7 <- ggplot(c6time)+
+ # geom_rect(aes(xmin= as.POSIXct(x_min), xmax= as.POSIXct(x_max), ymin=0, ymax=4500), fill ="#a35e60")+
+  #geom_point(aes(x= DateTime, y = V1), color = "#cecd7b")+
+ # geom_point(aes(x= DateTime, y = V2), color = "#1dace8")+
+ # ggtitle("Station 1 and 2 CO2")
+#plot7 <- plot7 + theme(axis.text.y= largernumbers)
+#ggplotly(plot7)
+
+plot7 <- ggplot(c6time)+
+  geom_point(aes(x= DateTime, y = V1), color = "#cecd7b")+
+  geom_point(aes(x= DateTime, y = V2), color = "#1dace8")+
+  ggtitle("Station 1 and 2 CO2")
+plot7 <- plot7 + theme(axis.text.y= largernumbers)
+ggplotly(plot7)
+
+
+#plot8 <- ggplot(c6time)+
+ # geom_point(aes(x= DateTime, y = V2), color = "#cecd7b")+
+  #ggtitle("Station 2 CO2")
+#plot8 <- plot8 + theme(axis.text.y= largernumbers)
+#ggplotly(plot8)
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(plot7),ggplotGrob(plot), ggplotGrob(plot2), ggplotGrob(plot3), ggplotGrob(plot4)))  
