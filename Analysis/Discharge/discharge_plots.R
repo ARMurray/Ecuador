798700690eccb0a493ef09f799087916b0940cfb
Andrew Murray <armurray@live.unc.edu>
2020-02-20 12:42:18 -0500

    working through updating to lubridate in stream_flux

--- Analysis/Discharge/discharge_plots.R
+++ Analysis/Discharge/discharge_plots.R
@@ -1,15 +1,18 @@
 ## Discharge plots
-
+library(lubridate)
+library(here)
 library(plotly)
 library(dplyr)
 library(ggplot2)
 
-df <- read.csv(here("data_4_analysis/All_Stream_Data.csv"))%>%
-  select(DateTime,Inj,V1,V2,V3,V4,Flux_1,Flux_2,tempC_436_m,airTemp_c,ppt24Tot,ppt48Tot,ppt72Tot,stn1_Q,stn2_Q,stn3_Q,stn4_Q)
-df$DateTime <- as.POSIXct(df$DateTime, format = "%m/%d/%Y %H:%M")
+df <- read.csv(here::here("data_4_analysis/All_Stream_Data.csv"))%>%
+  select(DateTime,stn1_Q,stn2_Q,stn3_Q,stn4_Q)
+df$DateTime <- ymd_hms(df$DateTime)
 
 
-stn1Q <- ggplot(df)+
-  geom_line(aes(x=DateTime, y= stn1_Q))
-
-ggplotly(stn1Q)
+df%>%
+  plot_ly()%>%
+  add_markers(x=~DateTime, y =~stn1_Q)%>%
+  add_markers(x=~DateTime, y =~stn2_Q)%>%
+  add_markers(x=~DateTime, y =~stn3_Q)%>%
+  add_markers(x=~DateTime, y =~stn4_Q)
