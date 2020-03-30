f5c3f53cb6177db37532bd33a1b6743806d470e3
Andrew Murray <armurray@live.unc.edu>
2020-03-30 14:23:55 -0400

    meg plot legend fix

--- Picarro/EOSTransects/cleaner_figure7.R
+++ Picarro/EOSTransects/cleaner_figure7.R
@@ -51,11 +51,10 @@ sat <- sat[order(sat$Sample),]
 #unsaturated plot 
 
 
-plotdry2 <- ggplot(unsat, mapping=aes(x= day, y= CorrectedAverage)) + 
-  geom_pointrange(unsat, mapping=aes(ymin= (CorrectedAverage-StdDev_iCO2), ymax=CorrectedAverage+StdDev_iCO2), group=unsat$Sample, size=.7, color="#6e8b3d", 
-                  shape=unsat$Sample)+scale_shape_manual(values=c(1,4,6,9)) 
-
-plotdry2 <- plotdry2 + ggtitle("Unsaturated")+
+plotdry2 <- ggplot(unsat)+ 
+  geom_pointrange(aes(x = day,y=CorrectedAverage, ymin= (CorrectedAverage-StdDev_iCO2), ymax=CorrectedAverage+StdDev_iCO2,shape=Sample), size=.7, color="#6e8b3d")+
+  scale_shape_manual(name = "Collar",values=c(1,4,6,9))+
+  ggtitle("Unsaturated")+
   labs(x="Day", y=expression(bold(paste(delta^{13}, "C", "F"[AQ], "[%]"))))+
   scale_x_discrete(limits = c("July17", "July23", "July30", "Aug02", 
                               "Aug09", "Aug13"),
@@ -63,19 +62,18 @@ plotdry2 <- plotdry2 + ggtitle("Unsaturated")+
                               "Aug09", "Aug13"))+
   theme_bw()+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.text.x = largernumbers2, axis.title.x=element_blank(),
-        axis.text.y=largernumbers2, axis.title.y=element_blank(),plot.title = element_text(margin = margin(t= 10, b = -20), face="bold"))
-  
-plotdry2 <- plotdry2 +  ylim(-18.2,-9)
+        axis.text.y=largernumbers2, axis.title.y=element_blank(),plot.title = element_text(margin = margin(t= 10, b = -20), face="bold"))+
+  ylim(-18.2,-9)
+
 plotdry2
 
 
 #saturated plot 
 
-plotwet2 <- ggplot(sat, mapping=aes(x= day, y= CorrectedAverage)) + 
-  geom_pointrange(sat, mapping=aes(ymin= (CorrectedAverage-StdDev_iCO2), ymax=CorrectedAverage+StdDev_iCO2), group=sat$Sample, size=.7, color="#a1ce5a", 
-                  shape=sat$Sample)+scale_shape_manual(values=c(16,15,18)) 
-
-plotwet2 <- plotwet2 + ggtitle("Saturated")+
+plotwet2 <- ggplot(sat) + 
+  geom_pointrange(mapping=aes(x= day, y= CorrectedAverage,ymin= (CorrectedAverage-StdDev_iCO2), ymax=CorrectedAverage+StdDev_iCO2,shape=Sample), size=.7, color="#a1ce5a")+
+  scale_shape_manual(name = "",values=c(16,15,18))+
+  ggtitle("Saturated")+
   labs(x="Day", y=expression(bold(paste(delta^{13}, "C", "F"[AQ], "[%]"))))+
   scale_x_discrete(limits = c("July17", "July23", "July30", "Aug02", 
                               "Aug09", "Aug13"),
@@ -83,9 +81,8 @@ plotwet2 <- plotwet2 + ggtitle("Saturated")+
                               "Aug09", "Aug13"))+
   theme_bw()+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.text.x = largernumbers2, axis.title.x=largernumbers,
-        axis.text.y=largernumbers2, axis.title.y=element_blank(),plot.title = element_text(margin = margin(t= 10, b = -20), face="bold"))
-
-plotwet2 <- plotwet2 +  ylim(-18.2,-9)
+        axis.text.y=largernumbers2, axis.title.y=element_blank(),plot.title = element_text(margin = margin(t= 10, b = -20), face="bold"))+
+  ylim(-18.2,-9)
 plotwet2
 
 
