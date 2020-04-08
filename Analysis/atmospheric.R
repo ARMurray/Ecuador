fa2f0e75a46b5d0776da5476206535217411148c
Andrew Roderick Murray <armurray@c130401-ood.ll.unc.edu>
2020-04-07 21:55:09 -0400

    atmoshpheric

---
+++ Analysis/atmospheric.R
@@ -0,0 +1,16 @@
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(here)
+
+df <- read.csv(here::here("data_4_analysis/Vaisala_Stations_AllData_Dirty.csv"))%>%
+  mutate(DateTime = ymd_hms(DateTime))
+
+
+atmospheric <- df%>%
+  filter(PPM<350)%>%
+  filter(PPM>200)%>%
+  filter(VID == "V2")
+
+ggplot(atmospheric)+
+  geom_line(aes(x=DateTime,  y=PPM))
