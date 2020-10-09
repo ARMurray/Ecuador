cb76fb2726dd65ac3596252c1837011d713c9007
Andrew Murray <armurray@live.unc.edu>
2020-10-09 12:13:50 -0400

    figures for Diego UNH talk

--- Analysis/Stream_Metabolism/sensitivity_analysis.R
+++ Analysis/Stream_Metabolism/sensitivity_analysis.R
@@ -1,9 +1,9 @@
-library(dplyr)
-library(ggplot2)
-library(plotly)
+library(tidyverse)
 library(lubridate)
 library(ggpubr)
 library(here)
+library(imputeTS)
+library(cowplot)
 
 #############
 # STATION 1 #
@@ -142,6 +142,42 @@ dev.off()
 #write.csv(df4, here("Analysis/Stream_Metabolism/ModelOutputs/stn4_summary.csv"))
 
 
+# TRY ADDING DISCHARGE
+
+
+stream <- read.csv("data_4_analysis/All_Stream_Data.csv")
+
+q <- stream%>%
+  select(DateTime, stn1_Q)%>%
+  drop_na()%>%
+  mutate(day = lubridate::date(DateTime))%>%
+  group_by(day)%>%
+  mutate(meanQ = mean(stn1_Q))%>%
+  ungroup()%>%
+  select(day,meanQ)%>%
+  distinct()
+
+
+# Create points for daily measured Flux
+flux <- stream%>%
+  filter(Inj.x == "No")%>%
+  select(DateTime,Flux_1)%>%
+  drop_na()%>%
+  mutate(DateTime = lubridate::ymd_hms(DateTime))
+
+## Impute missing values for every second of every day and then sum to get total flux each day.
+timestep <- hms("01:02:01")-hms("01:02:00")
+dailyFlux <- data.frame(time=seq.POSIXt(flux$DateTime[1], flux$DateTime[length(flux$DateTime)], by=timestep))%>%
+  left_join(flux, by = c("time"="DateTime"))%>%
+  na_interpolation(option = 'linear', maxgap = Inf)%>%
+  mutate(date = lubridate::date(time))%>%
+  group_by(date)%>%
+  mutate(dielFluxUmol = sum(Flux_1))%>%
+  ungroup()%>%
+  select(date,dielFluxUmol)%>%
+  distinct()%>%
+  mutate(gCO2day = dielFluxUmol* (44.01/1000000))   ## Convert to mass
+
 ### Add in precipitation to the figures
 ppt <- read.csv(here("data_4_analysis/ppt.csv"))%>%
   mutate(DateTime = lubridate::ymd(DateTime))
@@ -155,56 +191,69 @@ temp <- read.csv(here("data_4_analysis/airTemp_5min.csv"))%>%
   select(Day, Daily_High)%>%
   distinct()
 
-## STATION 1
-# GPP with ppt
-g1 <- ggplot(df)+
-  geom_line(data = temp, aes(x=Day,y=Daily_High/5), col = "#eb4034", linetype = "dashed")+
-  geom_segment(data = ppt, aes(x=DateTime,xend = DateTime, y = 3-(ppt_mm/20), yend = 3, group = DateTime), col = "#007FAE", size = 4)+
-  geom_line(aes(x= dateTime, y=GPP, group = ID),alpha = 0.1,size = 2)+
-  labs(x = "", y = bquote('GPP ['*g~ O[2]~ m^-2~d^-1*']'), title = "Station 1 GPP")+
-  geom_text(aes(x=ymd("20190810"), y = -.5, label = paste0("N = ",length(stn1Files))), size = 12)+
-  geom_hline(yintercept = 0)+
-  xlim(xmin = lubridate::ymd("2019-07-12"), xmax = lubridate::ymd("2019-08-15"))
+## COMBINED PLOTS
 
-# ER
-e1 <- ggplot(df)+
-  geom_line(data = temp, aes(x=Day,y=(Daily_High/5)-5), col = "#eb4034", linetype = "dashed")+
-  geom_segment(data = ppt, aes(x=DateTime,xend = DateTime, y = 0-(ppt_mm/20), yend = 0, group = DateTime), col = "#007FAE", size = 4)+
-  geom_line(aes(x= dateTime, y=ER, group = ID),alpha = 0.1,size = 2)+
-  labs(x = "", y = bquote('ER ['*g~ O[2]~ m^-2~d^-1*']'), title = "Station 1 ER")+
-  geom_text(aes(x=ymd("20190810"), y = -.5, label = paste0("N = ",length(stn1Files))), size = 12)+
+# RELATED VARIABLES
+var <- ggplot()+
+  geom_line(data = q, aes(x = day, y =meanQ), col = "purple", size=1)+
+  geom_line(data = temp, aes(x=Day,y=Daily_High/150), col = "#eb4034", linetype = "dashed",size=1)+
+  geom_segment(data = ppt, aes(x=DateTime,xend = DateTime, y = .075-(ppt_mm/700), yend = .075, group = DateTime), col = "#42aec9", size = 3, alpha = .7)+
   geom_hline(yintercept = 0)+
   xlim(xmin = lubridate::ymd("2019-07-12"), xmax = lubridate::ymd("2019-08-15"))+
-  ylim(ymin = -7, ymax=2)
+  scale_y_continuous(sec.axis = sec_axis( trans=~.*150, name="Max Daily Temp [C]"))+
+  labs(x = "", y = bquote('Discharge ['*m^-3~s^-1*']'), title = "")
+  
 
-e1
-er1 <- ggplot(df)+
-  geom_line(aes(x=dateTime,y=ER, group = ID),alpha = 0.1)+
-  labs(x = "", y = bquote('ER ['*g~ O[2]~ m^-2~d^-1*']'), title = "Station 1 ER")+
-  geom_text(aes(x=ymd("20190810"), y = -5.5, label = paste0("N = ",length(stn1Files))), size = 8)+
-  ylim(ymin = -7, ymax = 2)+
-  geom_hline(yintercept = 0)
+var  
+  
 
-## STATION 2
 # GPP with ppt
-g2 <- ggplot(df2)+
-  geom_line(data = temp, aes(x=Day,y=Daily_High/5), col = "#eb4034", linetype = "dashed")+
-  geom_segment(data = ppt, aes(x=DateTime,xend = DateTime, y = 3-(ppt_mm/20), yend = 3, group = DateTime), col = "#007FAE", size = 4)+
-  geom_line(aes(x= dateTime, y=GPP, group = ID),alpha = 0.1,size = 2)+
-  labs(x = "", y = bquote('GPP ['*g~ O[2]~ m^-2~d^-1*']'), title = "Station 1 GPP")+
-  geom_text(aes(x=ymd("20190810"), y = -.5, label = paste0("N = ",length(stn1Files))), size = 12)+
+gppdf <- rbind(df,df2)
+
+gpp <- ggplot()+
+  geom_line(data = df, aes(x= dateTime, y=GPP, group = ID),alpha = 0.1,size = 2, col = '#e0b43a')+
+  geom_line(data = df2, aes(x= dateTime, y=GPP, group = ID),alpha = 0.1,size = 2, col = '#64ab5b')+
+  labs(x = "", y = bquote('GPP ['*g~ O[2]~ m^-2~d^-1*']'), title = "Gross Primary Productivity")+
   geom_hline(yintercept = 0)+
-  xlim(xmin = lubridate::ymd("2019-07-12"), xmax = lubridate::ymd("2019-08-15"))
+  xlim(xmin = lubridate::ymd("2019-07-12"), xmax = lubridate::ymd("2019-08-15"))+
+  ylim(ymin = 0, ymax = 7)
+
+#gpp
 
 
-## STATION 4
-# GPP with ppt
-g3 <- ggplot(df4)+
-  geom_line(data = temp, aes(x=Day,y=Daily_High/5), col = "#eb4034", linetype = "dashed")+
-  geom_segment(data = ppt, aes(x=DateTime,xend = DateTime, y = 3-(ppt_mm/20), yend = 3, group = DateTime), col = "#007FAE", size = 4)+
-  geom_line(aes(x= dateTime, y=GPP, group = ID),alpha = 0.1,size = 2)+
-  labs(x = "", y = bquote('GPP ['*g~ O[2]~ m^-2~d^-1*']'), title = "Station 1 GPP")+
-  geom_text(aes(x=ymd("20190810"), y = -.5, label = paste0("N = ",length(stn1Files))), size = 12)+
+# ER
+er <- ggplot()+
+  geom_line(data = df, aes(x= dateTime, y=ER, group = ID),alpha = 0.1,size = 2, col = '#e0b43a')+
+  geom_line(data = df2, aes(x= dateTime, y=ER, group = ID),alpha = 0.1,size = 2, col = '#64ab5b')+
+  labs(x = "", y = bquote('ER ['*g~ O[2]~ m^-2~d^-1*']'), title = "Respiration")+
   geom_hline(yintercept = 0)+
-  xlim(xmin = lubridate::ymd("2019-07-12"), xmax = lubridate::ymd("2019-08-15"))
+  xlim(xmin = lubridate::ymd("2019-07-12"), xmax = lubridate::ymd("2019-08-15"))+
+  ylim(ymin = -7, ymax=0)
 
+ggarrange(var,gpp,er, nrow = 3)
+
+
+
+# Combined GPP and ER
+both <- ggplot()+
+  geom_line(data = df, aes(x= dateTime, y=GPP*(-1), group = ID),alpha = 0.1,size = 2, col = '#e0b43a')+
+  geom_line(data = df2, aes(x= dateTime, y=GPP*(-1), group = ID),alpha = 0.1,size = 2, col = '#64ab5b')+
+  geom_line(data = df, aes(x= dateTime, y=ER*(-1), group = ID),alpha = 0.1,size = 2, col = '#e0b43a')+
+  geom_line(data = df2, aes(x= dateTime, y=ER*(-1), group = ID),alpha = 0.1,size = 2, col = '#64ab5b')+
+  #geom_path(data = dailyFlux, aes(x = date, y = gCO2day),size = 2, col = "#3483eb")+
+  labs(x = "", y = bquote(''*g~ CO[2]~ m^-2~d^-1*''), title = "")+
+  geom_hline(yintercept = 0)+
+  geom_text(aes(x=ymd("20190722"), y = 2, label = paste0("Ecosystem Respiration")), size = 5)+
+  geom_text(aes(x=ymd("20190724"), y = -2.5, label = paste0("Gross Primary Productivity")), size = 5)+
+  #geom_text(aes(x=ymd("20190804"), y = 8.5, label = paste0("Evasion")), size = 5)+
+  xlim(xmin = lubridate::ymd("2019-07-12"), xmax = lubridate::ymd("2019-08-15"))+
+  ylim(ymin = -4, ymax = 11)
+both
+
+ggarrange(var,both,nrow=2)
+
+cowplot::plot_grid(var,both,nrow = 2, align = "h")
+
+library(grid)
+grid.newpage()
+grid.draw(rbind(ggplotGrob(var), ggplotGrob(both), size = "first"))
