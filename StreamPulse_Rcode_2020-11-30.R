625c3a200458a63580a4e96362e42aa004b1306b
kriddie <whitmore.kriddie@gmail.com>
2020-12-16 09:59:09 -0500

    comment out time change for  better data exploration

--- StreamPulse_Rcode_2020-11-30.R
+++ StreamPulse_Rcode_2020-11-30.R
@@ -6,7 +6,7 @@
 # stn 1:	72020436
 # stn 3:	72020421
 
-#DO notes
+#DO notesis
 #stn 1: 20645539
 #stn 2: 20645540
 #stn 4: 20645538
@@ -34,7 +34,6 @@ Other_Data <- subset(Other_Data, select=c(DateTime, V1_adjusted, V2_adjusted, V3
 colnames(Other_Data) <- c("Date_Time", "CO2_ppm_1","CO2_ppm_2","CO2_ppm_3","CO2_ppm_4",
          "Turbidity_NTU", "Chlorophylla_ug.L","CDOM_ppb","EC_uS_1", "EC2_uS_2", "EC4_uS_3", "Q_m3L")
 Other_Data$Date_Time <- as.POSIXct(Other_Data$Date_Time, format="%Y-%m-%d %H:%M:%S", tz="Etc/GMT-5")
-attr(Other_Data$Date_Time,"tzone") <- "UTC"
 
 #####weather station#####
 LaVirgin_p <- read.csv("C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/FieldData/Esteban/M5025_Precipitacion_2019-01-06_2020-02-01.csv",
@@ -42,7 +41,6 @@ LaVirgin_p <- read.csv("C:/Users/whitm/OneDrive - University of North Carolina a
 colnames(LaVirgin_p)=c("Date_Time","Precipt_cm")
 LaVirgin_p$Date_Time <- as.POSIXct(LaVirgin_p$Date_Time, format="%m/%d/%Y %H:%M", tz="Etc/GMT-5")
 LaVirgin_p$Date <- as.Date(LaVirgin_p$Date_Time)
-#attr(LaVirgin_p$Date_Time,"tzone") <- "UTC"
 
 #Sum precipitation by date
 LaVirgin_sum <- LaVirgin_p %>% 
@@ -62,7 +60,6 @@ LaVirgin_t$maximo <- NULL
 LaVirgin_t$minimo <- NULL
 colnames(LaVirgin_t)=c("Date_Time","Air_Temp_LaVirgin")
 LaVirgin_t$Date_Time <- as.POSIXct(LaVirgin_t$Date_Time, format="%m/%d/%Y %H:%M", tz="Etc/GMT-5")
-attr(LaVirgin_t$Date_Time,"tzone") <- "UTC"
 
 LaVirgin <- LaVirgin_t
   #merge(LaVirgin_p,LaVirgin_t, by = "Date_Time")
@@ -77,7 +74,6 @@ colnames(Baro)=c("Date","Time","offset","airpress_kpa","Air_Temp_c")
 Baro <- Baro[,c(1:2,4:5)]
 Baro$Date_Time <- paste(Baro$Date, Baro$Time)
 Baro$Date_Time <- as.POSIXct(Baro$Date_Time, format="%m/%d/%Y %H:%M:%S", tz="Etc/GMT-5")
-attr(Baro$Date_Time,"tzone") <- "UTC"
 Baro$Date <- NULL
 Baro$Time <- NULL
 
@@ -91,7 +87,6 @@ colnames(Baro_jan)=c("Date","Time","offset","airpress_kpa","Air_Temp_c")
 Baro_jan <- Baro_jan[,c(1:2,4:5)]
 Baro_jan$Date_Time <- paste(Baro_jan$Date, Baro_jan$Time)
 Baro_jan$Date_Time <- as.POSIXct(Baro_jan$Date_Time, format="%m/%d/%Y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(Baro_jan$Date_Time,"tzone") <- "UTC"
 Baro_jan$Date <- NULL
 Baro_jan$Time <- NULL
 
@@ -110,7 +105,6 @@ WL_stn1 <- WL_stn1[,c(1:2,4:5)]
 WL_stn1$Date_Time <- paste(WL_stn1$Date, WL_stn1$Time)
 
 WL_stn1$Date_Time <- as.POSIXct(WL_stn1$Date_Time, format="%m/%d/%Y %H:%M:%S", tz="Etc/GMT-5")
-attr(WL_stn1$Date_Time,"tzone") <- "UTC"
 WL_stn1$Date <- NULL
 WL_stn1$Time <- NULL
 
@@ -122,7 +116,6 @@ DO_stn1 <-
 DO_stn1 <- DO_stn1[,1:4]
 colnames(DO_stn1)=c("row","Date_Time","DO_mgl","DO_Temp")
 DO_stn1$Date_Time <- as.POSIXct(DO_stn1$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(DO_stn1$Date_Time,"tzone") <- "UTC"
 DO_stn1$DO_Temp <- (DO_stn1$DO_Temp - 32) * 5/9
 
 DO_stn1$row <- NULL
@@ -135,7 +128,6 @@ EC_stn1 <-
 EC_stn1 <- EC_stn1[,1:4]
 colnames(EC_stn1)=c("row","Date_Time","EC_us","EC_Temp")
 EC_stn1$Date_Time <- as.POSIXct(EC_stn1$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(EC_stn1$Date_Time,"tzone") <- "UTC"
 EC_stn1$EC_Temp <- (EC_stn1$EC_Temp - 32) * 5/9
 EC_stn1$row <- NULL
 
@@ -151,11 +143,12 @@ Stn1_Data_2019_08_14 <- left_join(Stn1_Data_2019_08_14, Baro, by="Date_Time")
 Stn1_Data_2019_08_14 <- left_join(Stn1_Data_2019_08_14, Other_Data_stn1, by="Date_Time")
 Stn1_Data_2019_08_14 <- left_join(Stn1_Data_2019_08_14,LaVirgin,  by="Date_Time")
 
-#Stn1_Data_2019_08_14$Date_Time <- format(Stn1_Data_2019_08_14$Date_Time, usetz=TRUE)
+# Write table
+#Don't forget to change to UTC!
 
+#attr(Stn1_Data_2019_08_14$Date_Time,"tzone") <- "UTC"
 #write_csv(Stn1_Data_2019_08_14, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU1_2019-08-14_XX.csv")
 
-# Write table
 #write.table(Stn1_Data_2019_08_14, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU1_2019-08-14_XX.csv", sep = ",", col.names = TRUE, row.names = FALSE)
 
 ### JANUARY DATA
@@ -174,7 +167,6 @@ WL_stn1$WL_m <- WL_stn1$WL_m*0.102 - .02
 
 WL_stn1$Date_Time <- paste(WL_stn1$Date, WL_stn1$Time)
 WL_stn1$Date_Time <- as.POSIXct(WL_stn1$Date_Time, format="%m/%d/%Y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(WL_stn1$Date_Time,"tzone") <- "UTC"
 WL_stn1$Date <- NULL
 WL_stn1$Time <- NULL
 
@@ -187,7 +179,6 @@ DO_stn1 <-
 DO_stn1 <- DO_stn1[,1:4]
 colnames(DO_stn1)=c("row","Date_Time","DO_mgl","DO_Temp")
 DO_stn1$Date_Time <- as.POSIXct(DO_stn1$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(DO_stn1$Date_Time,"tzone") <- "UTC"
 DO_stn1$DO_Temp <- (DO_stn1$DO_Temp - 32) * 5/9
 DO_stn1$row <- NULL
 
@@ -201,7 +192,6 @@ EC_stn1 <-
 EC_stn1 <- EC_stn1[,1:4]
 colnames(EC_stn1)=c("row","Date_Time","EC_us","EC_Temp")
 EC_stn1$Date_Time <- as.POSIXct(EC_stn1$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(EC_stn1$Date_Time,"tzone") <- "UTC"
 
 EC_stn1$row <- NULL
 
@@ -215,14 +205,14 @@ Stn1_Data_2020_01_19 <- full_join(Stn1_Data_2020_01_19, EC_stn1, by="Date_Time")
 Stn1_Data_2020_01_19 <- full_join(Stn1_Data_2020_01_19, Baro, by="Date_Time")
 Stn1_Data_2020_01_19 <- left_join(Stn1_Data_2020_01_19,LaVirgin, by="Date_Time")
 
-#Read out
+#Read out - don't forget to convert time zone for stream pulse
+
+#attr(Stn1_Data_2020_01_19$Date_Time,"tzone") <- "UTC"
 
 #Stn1_Data_2020_01_19$Date_Time <- format(Stn1_Data_2020_01_19$Date_Time, usetz=TRUE)
 #write_csv(Stn1_Data_2020_01_19, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU1_2020-01-19_XX.csv")
-
-#write.csv(Stn1_Data_2020_01_19, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU1_2020-01-19_XX.csv", row.names=FALSE)
-
-write.table(Stn1_Data_2020_01_19, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU1_2020-01-19_XX.csv", sep = ",", col.names = TRUE, row.names = FALSE)
+###use this one below
+#write.table(Stn1_Data_2020_01_19, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU1_2020-01-19_XX.csv", sep = ",", col.names = TRUE, row.names = FALSE)
 
 stn1_All <- rbind(Stn1_Data_2019_08_14,Stn1_Data_2020_01_19)
 
@@ -240,7 +230,6 @@ WL_stn2 <- WL_stn2[,c(1:2,4:5)]
 WL_stn2$Date_Time <- paste(WL_stn2$Date, WL_stn2$Time)
 
 WL_stn2$Date_Time <- as.POSIXct(WL_stn2$Date_Time, format="%m/%d/%Y %H:%M:%S", tz="Etc/GMT-5")
-attr(WL_stn2$Date_Time,"tzone") <- "UTC"
 WL_stn2$Date <- NULL
 WL_stn2$Time <- NULL
 
@@ -252,7 +241,6 @@ DO_stn2 <-
 DO_stn2 <- DO_stn2[,1:4]
 colnames(DO_stn2)=c("row","Date_Time","DO_mgl","DO_Temp")
 DO_stn2$Date_Time <- as.POSIXct(DO_stn2$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(DO_stn2$Date_Time,"tzone") <- "UTC"
 DO_stn2$DO_Temp <- (DO_stn2$DO_Temp - 32) * 5/9
 DO_stn2$row <- NULL
 
@@ -266,7 +254,6 @@ EC_stn2 <-
 EC_stn2 <- EC_stn2[,1:4]
 colnames(EC_stn2)=c("row","Date_Time","EC_us","EC_Temp")
 EC_stn2$Date_Time <- as.POSIXct(EC_stn2$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(EC_stn2$Date_Time,"tzone") <- "UTC"
 
 EC_stn2$row <- NULL
 
@@ -282,9 +269,10 @@ Stn2_Data_2019_08_14 <- left_join(Stn2_Data_2019_08_14, Baro, by="Date_Time")
 Stn2_Data_2019_08_14 <- left_join(Stn2_Data_2019_08_14,LaVirgin, by="Date_Time")
 
 #read out
-#Stn2_Data_2019_08_14$Date_Time <- format(Stn2_Data_2019_08_14$Date_Time, usetz=TRUE)
 
-write.csv(Stn2_Data_2019_08_14, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU2_2019-08-14_XX.csv", row.names=FALSE)
+#attr(Stn2_Data_2019_08_14$Date_Time,"tzone") <- "UTC"
+
+#write.csv(Stn2_Data_2019_08_14, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU2_2019-08-14_XX.csv", row.names=FALSE)
 
 
 
@@ -303,7 +291,6 @@ WL_stn2$Date_Time <- paste(WL_stn2$Date, WL_stn2$Time)
 WL_stn2$WL_m <- WL_stn2$WL_m*0.102 - .01
 
 WL_stn2$Date_Time <- as.POSIXct(WL_stn2$Date_Time, format="%m/%d/%Y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(WL_stn2$Date_Time,"tzone") <- "UTC"
 WL_stn2$Date <- NULL
 WL_stn2$Time <- NULL
 
@@ -315,7 +302,6 @@ DO_stn2 <-
 DO_stn2 <- DO_stn2[,1:4]
 colnames(DO_stn2)=c("row","Date_Time","DO_mgl","DO_Temp")
 DO_stn2$Date_Time <- as.POSIXct(DO_stn2$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(DO_stn2$Date_Time,"tzone") <- "UTC"
 DO_stn2$DO_Temp <- (DO_stn2$DO_Temp - 32) * 5/9
 DO_stn2$row <- NULL
 
@@ -329,7 +315,6 @@ EC_stn2 <-
 EC_stn2 <- EC_stn2[,1:4]
 colnames(EC_stn2)=c("row","Date_Time","EC_us","EC_Temp")
 EC_stn2$Date_Time <- as.POSIXct(EC_stn2$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(EC_stn2$Date_Time,"tzone") <- "UTC"
 
 EC_stn2$row <- NULL
 
@@ -340,10 +325,11 @@ Stn2_Data_2020_01_19 <- full_join(Stn2_Data_2020_01_19, EC_stn2, by="Date_Time")
 Stn2_Data_2020_01_19 <- left_join(Stn2_Data_2020_01_19, Baro, by="Date_Time")
 Stn2_Data_2020_01_19 <- left_join(Stn2_Data_2020_01_19,LaVirgin, by="Date_Time")
 
+#write out
 
-#Stn2_Data_2020_01_19$Date_Time <- format(Stn2_Data_2020_01_19$Date_Time, usetz=TRUE)
+#attr(Stn2_Data_2020_01_19$Date_Time,"tzone") <- "UTC"
 
-write.csv(Stn2_Data_2020_01_19, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU2_2020-01-19_XX.csv", row.names=FALSE)
+#write.csv(Stn2_Data_2020_01_19, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU2_2020-01-19_XX.csv", row.names=FALSE)
 
 stn2_All <- rbind(Stn2_Data_2019_08_14,Stn2_Data_2020_01_19)
 
@@ -363,7 +349,6 @@ DO_stn3 <-
 DO_stn3 <- DO_stn3[,1:4]
 colnames(DO_stn3)=c("row","Date_Time","DO_mgl","DO_Temp")
 DO_stn3$Date_Time <- as.POSIXct(DO_stn3$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(DO_stn3$Date_Time,"tzone") <- "UTC"
 DO_stn3$DO_Temp <- (DO_stn3$DO_Temp - 32) * 5/9
 DO_stn3$row <- NULL
 
@@ -377,7 +362,6 @@ EC_stn3 <-
 EC_stn3 <- EC_stn3[,1:4]
 colnames(EC_stn3)=c("row","Date_Time","EC_us","EC_Temp")
 EC_stn3$Date_Time <- as.POSIXct(EC_stn3$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(EC_stn3$Date_Time,"tzone") <- "UTC"
 EC_stn3$EC_Temp <- (EC_stn3$EC_Temp - 32) * 5/9
 
 EC_stn3$row <- NULL
@@ -388,11 +372,10 @@ Stn3_Data_2019_08_14 <- full_join(EC_stn3,DO_stn3,by="Date_Time")
 Stn3_Data_2019_08_14 <- left_join(Stn3_Data_2019_08_14, Baro, by="Date_Time")
 Stn3_Data_2019_08_14 <- left_join(Stn3_Data_2019_08_14,LaVirgin, by="Date_Time")
 
+#write out
+#attr(Stn3_Data_2019_08_14$Date_Time,"tzone") <- "UTC"
 
-#Stn3_Data_2019_08_14$Date_Time <- format(Stn3_Data_2019_08_14$Date_Time, usetz=TRUE)
-
-write_csv(Stn3_Data_2019_08_14, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU2_2019_08_14_XX.csv")
-#write.csv(Stn3_Data_2019_08_14, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU2_2019_08_14_XX.csv", row.names=FALSE)
+#write_csv(Stn3_Data_2019_08_14, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU2_2019_08_14_XX.csv")
 
 
 #### no wl
@@ -405,7 +388,6 @@ DO_stn3 <-
 DO_stn3 <- DO_stn3[,1:4]
 colnames(DO_stn3)=c("row","Date_Time","DO_mgl","DO_Temp")
 DO_stn3$Date_Time <- as.POSIXct(DO_stn3$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(DO_stn3$Date_Time,"tzone") <- "UTC"
 DO_stn3$DO_Temp <- (DO_stn3$DO_Temp - 32) * 5/9
 DO_stn3$row <- NULL
 
@@ -419,7 +401,6 @@ EC_stn3 <-
 EC_stn3 <- EC_stn3[,1:4]
 colnames(EC_stn3)=c("row","Date_Time","EC_us","EC_Temp")
 EC_stn3$Date_Time <- as.POSIXct(EC_stn3$Date_Time, format="%m/%d/%y %I:%M:%S %p", tz="Etc/GMT-5")
-attr(EC_stn3$Date_Time,"tzone") <- "UTC"
 
 EC_stn3$row <- NULL
 
@@ -429,8 +410,9 @@ Stn3_Data_2020_01_19 <- full_join(DO_stn3,EC_stn3,by="Date_Time")
 Stn3_Data_2020_01_19 <- left_join(Stn3_Data_2020_01_19, Baro, by="Date_Time")
 Stn3_Data_2020_01_19 <- left_join(Stn3_Data_2020_01_19,LaVirgin, by="Date_Time")
 
-
-write_csv(Stn3_Data_2020_01_19, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU2_2020_01_19_XX.csv")
+#write out
+#attr(Stn3_Data_2020_01_19$Date_Time,"tzone") <- "UTC"
+#write_csv(Stn3_Data_2020_01_19, "C:/Users/whitm/OneDrive - University of North Carolina at Chapel Hill/Ecuador/Ecuador/StreamPulse/EC_IRU2_2020_01_19_XX.csv")
 
 stn3_All <- rbind(Stn3_Data_2019_08_14,Stn3_Data_2020_01_19)
 
